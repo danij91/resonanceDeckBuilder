3f546e28f3d20b9d3af68544cb0eb9728dd59138
Ralf <danij9112@gmail.com>
2025-04-06 21:02:32 +0900

    Feat: Add screenshot function

---
+++ components/photo-mode-container.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import type React from "react"
+
+import { useRef } from "react"
+import { ScreenshotButton } from "./screenshot-button"
+
+interface PhotoModeContainerProps {
+  isPhotoMode: boolean
+  children: React.ReactNode
+}
+
+export function PhotoModeContainer({ isPhotoMode, children }: PhotoModeContainerProps) {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <div
+      ref={containerRef}
+      className={`transition-all duration-300 ${isPhotoMode ? "photo-mode bg-black py-4 px-2" : ""}`}
+    >
+      {children}
+      <ScreenshotButton targetRef={containerRef} isPhotoMode={isPhotoMode} />
+    </div>
+  )
+}
+
